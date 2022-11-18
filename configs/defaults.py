1efbf8ce204d69c04c6eac17702cf4438189df33
tr2512 <45138961+tr2512@users.noreply.github.com>
2022-11-18 10:44:27 +0700

    Fix directory and device on training scripts

--- configs/defaults.py
+++ configs/defaults.py
@@ -4,12 +4,12 @@ from yacs.config import CfgNode as CN
 
 _C = CN()
 
-_C.OUTPUT_DIR = "./SemiSegmen20221/logs/train_fully"
+_C.OUTPUT_DIR = "logs/train_fully"
 _C.MODEL = CN()
 _C.MODEL.NAME = "deeplabv3+_resnet50"
 _C.MODEL.NUM_CLASSES = 21
 _C.MODEL.ATROUS = [6, 12, 18]
-_C.MODEL.DEVICE = "cpu"
+_C.MODEL.DEVICE = "cuda:0"
 _C.MODEL.WEIGHTS = ""
 _C.MODEL.FREEZE_BN = False
 
@@ -19,12 +19,12 @@ _C.INPUT.CROP_SIZE = (512, 512)
 _C.INPUT.FLIP_PROB = 0.5
 
 _C.DATASETS = CN()
-_C.DATASETS.TRAIN_IMGDIR = "../input/pascal/pascal_voc/train_aug/image"
-_C.DATASETS.TRAIN_LBLDIR = "../input/pascal/pascal_voc/train_aug/label"
-_C.DATASETS.VAL_IMGDIR = "../input/pascal/pascal_voc/val/image"
-_C.DATASETS.VAL_LBLDIR = "../input/pascal/pascal_voc/val/label"
-_C.DATASETS.LABEL_LIST = "../input/pascal/pascal_voc/subset_train_aug/train_aug_labeled_1-8.txt"
-_C.DATASETS.UNLABELLED_LIST = "../input/pascal/pascal_voc/subset_train_aug/train_aug_unlabeled_1-8.txt"
+_C.DATASETS.TRAIN_IMGDIR = "pascal_voc/train_aug/image"
+_C.DATASETS.TRAIN_LBLDIR = "pascal_voc/train_aug/label"
+_C.DATASETS.VAL_IMGDIR = "pascal_voc/val/image"
+_C.DATASETS.VAL_LBLDIR = "pascal_voc/val/label"
+_C.DATASETS.LABEL_LIST = "pascal_voc/subset_train_aug/train_aug_labeled_1-8.txt"
+_C.DATASETS.UNLABELLED_LIST = "pascal_voc/subset_train_aug/train_aug_unlabeled_1-8.txt"
 
 _C.SOLVER = CN()
 _C.SOLVER.MAX_ITER = 32000
