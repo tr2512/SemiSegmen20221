396e2f4d72edc60c9f07fb0d384392bb4890323d
trid4012 <trid4012@gmail.com>
2022-12-22 22:06:01 +0700

    add soft_label

--- soft_label.py
+++ soft_label.py
@@ -40,7 +40,6 @@ def train(cfg, logger, pretrain = None ,checkpoint = None, output_dir= None, epo
     teacher_model = convert.convert_dsbn(teacher_model)
     teacher_model.load_state_dict(saved['model_state_dict'])
     teacher_model.to(device)
-    teacher_model.eval()
     
     iteration = 0
 
@@ -49,7 +48,7 @@ def train(cfg, logger, pretrain = None ,checkpoint = None, output_dir= None, epo
     if checkpoint:
         checkpoint_save = torch.load(checkpoint)
         model.load_state_dict(checkpoint_save['model_state_dict'])
-        iteration = checkpoint_save["iteration"]
+        iteration = checkpoint_save['iter']
     model.to(device)
 
     max_iter = 80000
@@ -59,26 +58,12 @@ def train(cfg, logger, pretrain = None ,checkpoint = None, output_dir= None, epo
     lr=cfg.SOLVER.LR, momentum=cfg.SOLVER.MOMENTUM, weight_decay=cfg.SOLVER.WEIGHT_DECAY)
     optimizer.zero_grad()
 
-    
-
     #Load datasets
     lbl_img_list = read_file(cfg.DATASETS.LABEL_LIST)
     ulbl_img_list = read_file(cfg.DATASETS.UNLABELLED_LIST)
 
-    lbl_list = []
-    ulbl_list = []
-    while len(lbl_list) < max_iter:
-        lbl_list += lbl_img_list
-        random.shuffle(lbl_img_list)
-    lbl_list = lbl_list[:max_iter]
-    
-    while len(ulbl_list) < max_iter:
-        ulbl_list += ulbl_img_list
-        random.shuffle(ulbl_img_list)
-    ulbl_list = ulbl_list[:max_iter]
-    
     lbl_train_data = VOCDataset(cfg.DATASETS.TRAIN_IMGDIR, cfg.DATASETS.TRAIN_LBLDIR,
-                            img_list=lbl_list,
+                            img_list=lbl_img_list,
                             transformation=Compose([
                             ToTensor(), 
                             Normalization(), 
@@ -87,7 +72,7 @@ def train(cfg, logger, pretrain = None ,checkpoint = None, output_dir= None, epo
                             RandomFlip(cfg.INPUT.FLIP_PROB)]))
 
     ulbl_train_data = VOCDataset(cfg.DATASETS.TRAIN_IMGDIR, cfg.DATASETS.TRAIN_LBLDIR,
-                            img_list=ulbl_list,
+                            img_list=ulbl_img_list,
                             transformation=Compose( list(SDA(cfg.INPUT.SDA)) + [ 
                             ToTensor(), 
                             Normalization(), 
@@ -140,18 +125,18 @@ def train(cfg, logger, pretrain = None ,checkpoint = None, output_dir= None, epo
             model.train()
             data_time = time.time() - end
             end = time.time()
-            
+
             optimizer.param_groups[0]['lr'] = (cfg.SOLVER.LR/4) * (1 - iteration/max_iter)**cfg.SOLVER.POWER
-        
+
             optimizer.zero_grad()
             images = images.to(device)
             labels = labels.to(device)
             
             u_imgs = u_imgs.to(device)
-            
+
             with torch.no_grad():
                 u_labels = teacher_model(u_imgs)
-                u_labels = u_labels.argmax(dim=1)
+                u_labels = nn.Softmax(dim=1)(u_labels)
             images = torch.cat((images, u_imgs), dim = 0).to(device)
             
             preds = model(images)
@@ -216,7 +201,7 @@ def train(cfg, logger, pretrain = None ,checkpoint = None, output_dir= None, epo
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Pytorch training")
     parser.add_argument("--pretrain", default="")
-    parser.add_argument("--checkpoint", default=None)
+    parser.add_argument("--checkpoint", default="")
     parser.add_argument("--output_dir", default="")
     parser.add_argument("--epoch", default=None)
     args = parser.parse_args()
